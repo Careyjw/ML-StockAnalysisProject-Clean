c17c7de944281c5a4bf5c197e62a23bf82736082
Colton Freitas <freitacr@plu.edu>
2018-11-27 11:20:50 -0800

    Added Source Download Methods and Docstrings
    Added DataDownloadSources, including methods for downloading data from
    both sources and appropriate docstrings

---
+++ src/StockDataDownloadMain.py
@@ -0,0 +1,8 @@
+'''
+Created on Nov 27, 2018
+
+@author: Colton Freitas
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ src/StockDataDownloader/DataDownloadSources.py
@@ -0,0 +1,26 @@
+'''
+Created on Nov 27, 2018
+
+@author: Colton Freitas
+
+'''
+
+
+
+def DownloadDataYahoo (tickerList):
+    '''Downloads Stock Data from Yahoo
+        @param tickerList: The list of stock tickers to obtain data for
+        @type tickerList: Array of Strings
+        @return Storage object containing obtained data
+        @rtype: StockDataStorage
+    '''
+    pass;
+    
+def DownloadDataGoogle (tickerList):
+    '''Downloads Stock Data from Google
+        @param tickerList: The list of stock tickers to obtain data for
+        @type tickerList: Array of Strings
+        @return Storage object containing obtained data
+        @rtype: StockDataStorage
+    '''    
+    pass;
