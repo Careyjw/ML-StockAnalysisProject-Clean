288b6d561246272f9d03817c1503805b2e2ef196
Colton Freitas <freitacr@plu.edu>
2018-12-05 16:15:49 -0800

    Documentation and Print Statement Cleanup
    Add documentation for most methods
    Removed all unused print statements (based off of whether the statement was commented out or not

--- src/StockDataPrediction/MachineLearningModels/SingleDataCateogryRNN.py
+++ src/StockDataPrediction/MachineLearningModels/SingleDataCateogryRNN.py
@@ -78,39 +78,54 @@ class RNNNormal:
                 
             return [o, s, a]
 
-        def calculate_total_loss(self, x, y):
+        def __calculate_total_loss(self, x, y):
+            '''Calculates the loss value for training evaluation
+            :param x: Training values
+            :type x: List of List of Num
+            :param y: Target values
+            :type y: List of Num
+            '''
             num_training_examples = np.sum((len(y_i) for y_i in y))
             
-            #print("Number training examples:", num_training_examples)
-            
-            #print(self.calculate_loss(x, y))
-            
-            return self.calculate_loss(x, y) / num_training_examples
+            return self.__calculate_loss(x, y) / num_training_examples
         
-        def calculate_loss (self, x, y):
+        def __calculate_loss (self, x, y):
+            '''Calculates the loss value for training evaluation
+            :param x: Current Training Example
+            :type x: List of Num
+            :param y: Current Target Value
+            :type y: Num
+            '''
             L = 0
             
             for i in np.arange(len(y)):
                 o,s,a = self.forward_prop(x[i])
                 
                 correct_word_predictions = o[np.arange(len(y[i])), y[i]]
-                #print("y[i]:", y[i])
-                
-                #print("Correct Predictions:", correct_word_predictions)
-                #print("O:", o)
                 
                 L += -1 * np.sum(np.log(correct_word_predictions))
             return L
         
         def predict (self, x):
+            '''Makes a prediction based off of the given training example
+            :param x: Data to make prediction from
+            :type x: List of Num
+            '''
             o,s,a = self.forward_prop(x)
             return self.deNormFunc(np.argmax(o, axis=1)[0])
             
-        def backPropogation (self, x, y):
+        def __backPropogation (self, x, y):
+            '''Performs forward and truncated backpropogation on the RNN
+            :param x: Training example to use
+            :type x: List of Num
+            :param y: Target examples to use
+            :type y: List of Num
+
+            TODO: Clean function by splitting into sub functions
+            '''
             y = [self.normFunc(v) for v in y]
             time_steps = len(y)
             o,s,a = self.forward_prop(x);
-            #print("Survived Forward Propagation")
             dLdU = np.zeros(self.U.shape)
             dLdV = np.zeros(self.V.shape)
             dLdW = np.zeros(self.W.shape)
@@ -132,15 +147,9 @@ class RNNNormal:
             for t in np.arange(time_steps)[::-1]:
                 #Partial previously calculated multiplied by s[t]
                 dLdV += np.outer(delta_o[t], s[t])
-                #print("Staring a time step...")
-                #print("delta_o.T[t]:",delta_o.T[t])
-                #print("delta_o[t].T:",delta_o[t].T)
                 current_partial_base = self.V.T.dot(delta_o[t].T)
-                #print("Calculated initial partial base")
                 current_partial_base = current_partial_base.T * (1 - s[t] ** 2)
-                #print("Updated partial base...")
                 for (back_prop_step) in np.arange(max(0, t-self.backPropTruncateAmount), t+1)[::-1]:
-                    #print("Starting Backtracing Step...")
                     dLdW += np.outer(s[back_prop_step-1], current_partial_base)
                    
                     dLdU += np.outer(a[back_prop_step], current_partial_base)
@@ -153,22 +162,18 @@ class RNNNormal:
                         dLdWeights[i] += partial_temp_storage * current_transformed_x[i]
 
                     current_partial_base = self.W.T.dot(current_partial_base) *  (1 - s[back_prop_step] ** 2)
-                    #print("Survived a backtracing step...")
-                #print("Survived a time step...")
             self.V -= dLdV * self.learning_rate
             self.W -= dLdW * self.learning_rate
             self.U -= dLdU * self.learning_rate
             self.inputWeights -= dLdWeights * self.learning_rate
             
-            return [dLdV, dLdW, dLdU, dLdWeights];
+            return [dLdV, dLdW, dLdU, dLdWeights]
         
         def train(self, x, y, verbose=False, batch_gradient_descent = False, batch_gradient_size = 100, eval_loss_after = 5, train_until_convergence = True, num_epochs = 20):
-            #print(x[0], y[0])
             
-            bgd = True if batch_gradient_descent else False; # So this is the conditional statement in python's language. I only pray it translates into assembly
-                #the nice way that C based language's conditional statements do.
-                #This also takes care of the case when batch_gradient_descent is neither True nor False (aka 5, None, etc.)
-            verbose = True if verbose else False;
+            bgd = True if batch_gradient_descent else False; 
+                #This takes care of the case when batch_gradient_descent is neither True nor False (aka 5, None, etc.)
+            verbose = True if verbose else False
                 
             if (train_until_convergence):
                 if (bgd):
@@ -177,7 +182,6 @@ class RNNNormal:
                     self.__complete_training(self.__full_step, x, y, verbose, eval_loss_after, ())
             else:
                 if (bgd):
-                    #print("Entering Epoch Training")
                     self.__epoch_training(self.__batch_step, x, y, verbose, eval_loss_after, num_epochs, (batch_gradient_size))
                 else:
                     self.__epoch_training(self.__full_step, x, y, verbose, eval_loss_after, num_epochs, ())
@@ -188,14 +192,12 @@ class RNNNormal:
             random.shuffle(batch_indices)
             batch_indices = batch_indices[:batch_size]
             
-            #print("Batch indices obtained...")
-            
             for index in batch_indices:
-                self.backPropogation(x[index], y[index])
+                self.__backPropogation(x[index], y[index])
                 
         def __full_step(self, x, y):
             for index in range(len(y)):
-                self.backPropogation(x[index], y[index])
+                self.__backPropogation(x[index], y[index])
                 
         def __complete_training(self, step_method, x, y, verbose, eval_loss_after, step_args):
             epochs = 0
@@ -203,7 +205,7 @@ class RNNNormal:
                 step_method(x, y, step_args)
                 epochs += 1
                 if (epochs % eval_loss_after == 0):
-                    self.losses.extend([self.calculate_total_loss(x, y)])
+                    self.losses.extend([self.__calculate_total_loss(x, y)])
                     if (len(self.losses) > 1):
                         if (self.losses[-1] > self.losses[-2]):
                             self.learning_rate *= .5
@@ -219,7 +221,7 @@ class RNNNormal:
                 step_method(x, y, step_args)
                 curr_epoch += 1
                 if (curr_epoch % eval_loss_after == 0):
-                    self.losses.extend([self.calculate_total_loss(x, y)])
+                    self.losses.extend([self.__calculate_total_loss(x, y)])
                     if (len(self.losses) > 1):
                         if (self.losses[-1] > self.losses[-2]):
                             self.learning_rate *= .5
@@ -243,7 +245,6 @@ class RNNNormal:
             self.__storeMatrix(self.inputWeights, open_file)
             
             open_file.close()
-            pass
         
         def load(self, filePath):
             open_file = open(filePath, "r")
@@ -259,9 +260,13 @@ class RNNNormal:
             self.inputWeights = (self.__loadMatrix(open_file))
             
             open_file.close()
-            pass
         
         def __storeMatrix(self, mat, file_handle):
+            '''Stores the matrix specified by mat in the file specified by file_handle
+            :param mat: Rectangular, two dimensional numpy matrix
+            :param file_handle: open file handle to the file to write the matrix to
+            Designed to be used with __loadMatrix to handle model I/O
+            '''
             #assumes matrix is 2 dimensional and is rectangular
             file_handle.write("%d %d\n" % (len(mat), len(mat[0])))
             for row in mat:
@@ -274,6 +279,12 @@ class RNNNormal:
                 file_handle.write(format_string % tuple([x for x in row]))
         
         def __loadMatrix(self, file_handle):
+            '''Loads a matrix from the file specified by file_handle and returns it
+            :param file_handle: open file handle to read the matrix from
+            :return: Numpy matrix containing the rectangular, two dimensional matrix read from file_handle
+            Designed to be used with __storeMatrix to handle model I/O            
+            '''
+            
             rows, cols = file_handle.readline().split(" ")
             ret = np.zeros( (int(rows), int(cols)) )
             for row in range(int(rows)):
