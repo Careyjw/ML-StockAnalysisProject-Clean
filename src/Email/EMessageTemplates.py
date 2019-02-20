2408853c3d8603f4fb8b093128be748911a18d28
Freitacr <freitacr@plu.edu>
2019-02-20 13:44:20 -0800

    Update Email Template
    Update Email Template formatting

--- src/Email/EMessageTemplates.py
+++ src/Email/EMessageTemplates.py
@@ -6,9 +6,11 @@ def _initializeMessageTemplates():
     global devTickerPredEM
     devTickerPredEM = EMessage(
     '''Hello {customer},
-    Here is your prediction results for today:
-    {ticker}
-    Have a nice day''', "prediction", "Daily Prediction Results", [], defaultMessageStatus=False)
+Here is your prediction results for today:
+
+{ticker}
+
+Have a nice day''', "prediction", "Daily Prediction Results", [], defaultMessageStatus=False)
 
 _initializeMessageTemplates()
 
