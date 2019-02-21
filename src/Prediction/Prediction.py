78a120f326e855014f71f5b8fc69bf74c64eab69
Careyjw <34852086+Careyjw@users.noreply.github.com>
2019-02-21 12:04:29 -0800

    implement AssembleEmail
    closes #27, #24

--- src/Prediction/Prediction.py
+++ src/Prediction/Prediction.py
@@ -3,23 +3,34 @@ from Common.CommonValues import modelStoragePathBase
 from Common.Util.CommonFunctions import getModelFiles
 from Data.Structures.CaselessDictionary import CaselessDictionary
 from AI.StoredModelFile import StoredModelFile
+from typing import List
+from Email.EMessageTemplates import devTickerPredEM
+from Email.EMessage import EMessage
 
 #methods
 def LoadModel(loginCredentials:List[str]) -> List["StoredModelFile"]:
     ModelPaths=getModelFiles(modelStoragePathBase)
     models=[]
     for path in ModelPaths:
-        modelConfiguration = caselessDictionary()
+        modelConfiguration = CaselessDictionary()
         modelConfiguration['General'] = CaselessDictionary()
         modelConfiguration['General']['lsLoginCredentials'] = loginCredentials
         models.append(StoredModelFile.Load(path,modelConfiguration))
     return models
-def predict(models:List["StoredModelFile"]) -> List[tuple]:
+
+def predictModel(models:List["StoredModelFile"]) -> List[tuple]:
     predictions=[]
     for model in models:
         prediction=model.Predict()
         predictions.append((model,prediction))
     return predictions
-#template
 
+def AssembleEmail(predictions: List[tuple]) -> "EMessage":
+    baseString = "{modID} version of {ticker} trained for {epochs} epochs predicted value: {predicted}"
+    NumTick=len(predictions)
+    retTemplate=devTickerPredEM.multiplyKey("{ticker}",NumTick)
+    for model,predicted in predictions:
+        modPredString= baseString.format(ticker=model.Ticker, modID=model.ModelID, epochs=int(model.Epochs), predicted=predicted)
+        retTemplate=retTemplate.replaceKey("{ticker}", modPredString)
+    return retTemplate    
 #pushEmail
